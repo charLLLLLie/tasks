216dace64d08ec1078e4a1de708a05ed5d75111b
charLLLLLie <mega.joker00@gmail.com>
2021-07-16 23:41:28 +0300

    added sending analytics

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -161,9 +161,24 @@ class App {
         const activeProjectsList = new ProjectList('active')
         const finishedProjectsList = new ProjectList('finished')
         activeProjectsList.setSwitchHandlerFunction(
-            finishedProjectsList.addProject.bind(finishedProjectsList))
+            finishedProjectsList.addProject.bind(finishedProjectsList)
+        )
         finishedProjectsList.setSwitchHandlerFunction(
-            activeProjectsList.addProject.bind(activeProjectsList))
+            activeProjectsList.addProject.bind(activeProjectsList)
+        )
+
+        const timerId = setTimeout(this.startAnalytics, 3000)
+
+        document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+            clearTimeout(timerId)
+        })
+    }
+
+    static startAnalytics() {
+        const analyticsScript = document.createElement('script')
+        analyticsScript.src = 'assets/scripts/analytics.js'
+        analyticsScript.defer = true
+        document.head.append(analyticsScript)
     }
 }
 
